ed1061c14b4b4dc65c6fa7874eb2166fb0b63003
DucNM <manhducnguyen.it@gmail.com>
2019-11-24 21:22:51 +0700

    change name

--- app/src/androidTest/java/com/example/anhvan/appnote/ExampleInstrumentedTest.java
+++ app/src/androidTest/java/com/example/anhvan/appnote/ExampleInstrumentedTest.java
@@ -21,6 +21,6 @@ public class ExampleInstrumentedTest {
         // Context of the app under test.
         Context appContext = InstrumentationRegistry.getTargetContext();
 
-        assertEquals("com.example.anhminh.appnote", appContext.getPackageName());
+        assertEquals("com.example.anhvan.appnote", appContext.getPackageName());
     }
 }
